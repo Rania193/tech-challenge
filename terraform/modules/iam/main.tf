d64c71c127511bba3ce45a4e13215bdaff53ec9a
Rania193 <rania.fahmy222@gmail.com>
2025-06-13 20:57:55 +0300

    restricting ecr pushes to certain repos, adding oidc provider, restricting github action role to certain repo and branch main

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,33 +1,104 @@
-resource "aws_iam_user" "service_user" {
-  name = "${var.project_name}-service-user-${var.environment}"
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+# OIDC provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"] # GitHub's OIDC thumbprint
 }
 
-resource "aws_iam_user_policy" "service_policy" {
-  name   = "${var.project_name}-service-policy"
-  user   = aws_iam_user.service_user.name
+# IAM user for microservices
+resource "aws_iam_user" "app_user" {
+  name = "${var.project_name}-user-${var.environment}"
+  tags = {
+    Name        = "${var.project_name}-user"
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_access_key" "app_user_key" {
+  user = aws_iam_user.app_user.name
+}
+
+resource "aws_iam_user_policy" "app_user_policy" {
+  name   = "${var.project_name}-user-policy-${var.environment}"
+  user   = aws_iam_user.app_user.name
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "s3:ListAllMyBuckets",
           "s3:GetBucketLocation"
         ]
         Resource = "*"
       },
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "ssm:GetParameters",
           "ssm:DescribeParameters"
         ]
-        Resource = "*"
+        Resource = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.project_name}/${var.environment}/*"
       }
     ]
   })
 }
 
-resource "aws_iam_access_key" "service_key" {
-  user = aws_iam_user.service_user.name
+# IAM role for GitHub Actions with OIDC trust
+resource "aws_iam_role" "github_actions_role" {
+  name = "${var.project_name}-github-actions-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-github-actions"
+    Environment = var.environment
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_policy" {
+  name   = "${var.project_name}-github-actions-policy-${var.environment}"
+  role   = aws_iam_role.github_actions_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+        Resource = [
+          "arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/main-api",
+          "arn:aws:ecr:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:repository/auxiliary-service"
+        ]
+      }
+    ]
+  })
 }
--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -7,3 +7,8 @@ variable "environment" {
   description = "Environment for IAM resources"
   type        = string
 }
+
+variable "github_repo" {
+  description = "GitHub repository in the format owner/repo"
+  type        = string
+}
