1357b168c8a299ced8e50c38aae10e3931a24248
Rania193 <rania.fahmy222@gmail.com>
2025-06-13 16:31:55 +0300

    added new output

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -2,3 +2,7 @@ output "state_bucket_name" {
   description = "Name of the Terraform state S3 bucket"
   value       = aws_s3_bucket.state_bucket.bucket
 }
+output "state_lock_table" {
+  description = "Name of the DynamoDB table for state locking"
+  value       = aws_dynamodb_table.state_lock.name
+}
