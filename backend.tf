2bd48da76e0fde0d000a8fdb80794dac3e448d17
Rania193 <rania.fahmy222@gmail.com>
2025-06-13 16:37:14 +0300

    edited gitignore and added dynamodb table

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "kantox-terraform-state"
-    key            = "challenge/terraform.tfstate"
-    region         = "eu-west-1"
-    use_lockfile   = true # S3 native locking
-  }
-}
