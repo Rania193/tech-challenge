3c5513af6fdc569f44d1b3505de8ef9e2525a731
Rania193 <rania.fahmy222@gmail.com>
2025-06-13 21:05:22 +0300

    changed names in outputs

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,11 +1,11 @@
 output "access_key_id" {
   description = "Access key ID for the IAM user"
-  value       = aws_iam_access_key.service_key.id
+  value       = aws_iam_access_key.app_user_key.id
   sensitive   = true
 }
 
 output "secret_access_key" {
   description = "Secret access key for the IAM user"
-  value       = aws_iam_access_key.service_key.secret
+  value       = aws_iam_access_key.app_user_key.secret
   sensitive   = true
 }
