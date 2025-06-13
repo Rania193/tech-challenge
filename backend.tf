c631a5d8282b279c2a6b0ae8f09cddc179cf21d7
Rania193 <rania.fahmy222@gmail.com>
2025-06-13 16:21:39 +0300

    added git ignore, fixed syntax issue, and uncommented backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "kantox-terraform-state"
-#     key            = "challenge/terraform.tfstate"
-#     region         = "eu-west-1"
-#     use_lockfile   = true # S3 native locking
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "kantox-terraform-state"
+    key            = "challenge/terraform.tfstate"
+    region         = "eu-west-1"
+    use_lockfile   = true # S3 native locking
+  }
+}
