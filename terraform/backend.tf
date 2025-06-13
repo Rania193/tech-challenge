5fc6500e0b3f1571a92427b639c406f75cfb28fe
Rania193 <rania.fahmy222@gmail.com>
2025-06-13 16:50:10 +0300

    added dynamodb table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "kantox-terraform-state"
+    bucket         = "kantox-challenge-terraform-state"
     key            = "challenge/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "kantox-terraform-lock"
+    dynamodb_table = "kantox-challenge-terraform-lock"
   }
 }
