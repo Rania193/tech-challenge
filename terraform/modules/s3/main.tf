6015c92644236652c68678505116fad0c1940552
Rania193 <rania.fahmy222@gmail.com>
2025-06-13 15:37:13 +0300

    fixed depricated blocks, blocking public access

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -2,20 +2,32 @@
 resource "aws_s3_bucket" "challenge_bucket" {
   bucket = "${var.project_name}-bucket-${var.environment}"
 
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   tags = {
     Name        = "${var.project_name}-bucket"
     Environment = var.environment
   }
+}
+resource "aws_s3_bucket_versioning" "challenge_bucket_versioning" {
+  bucket = aws_s3_bucket.challenge_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "challenge_bucket_encryption" {
+  bucket = aws_s3_bucket.challenge_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "challenge_bucket_access" {
+  bucket                  = aws_s3_bucket.challenge_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
