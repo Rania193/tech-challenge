f35458dc235cc53d47553afb65968cfa67584d47
Rania193 <rania.fahmy222@gmail.com>
2025-06-14 23:56:19 +0300

    added permissions + fingerprint

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -5,7 +5,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"] # GitHub's OIDC thumbprint
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's OIDC thumbprint
 }
 
 # IAM user for microservices
